f7ed2bbb7153cbd1e778701327c4e3e6c83c8a9a
Alan Chandler <alan@chandlerfamily.org.uk>
2008-03-15 15:07:10 +0000

    Update version as not working :cry

--- mbchat.js
+++ mbchat.js
@@ -1,5 +1,5 @@
 MBchat = function () {
-	var version = 'v0.9.2';
+	var version = 'v0.9.3';
 	var me;
 	var myRequestOptions;
 	var entranceHall;  //Entrance Hall Object
@@ -26,7 +26,8 @@ MBchat = function () {
 	};
 return {
 	init : function(user,pollOptions,chatBotName, entranceHallName, msgLstSz) {
-		$('version').set('text', version);
+		var span = $('version');
+		span.set('text', version);
 		
 /*		soundManager.onload = function() {
 			soundManager.createSound({
