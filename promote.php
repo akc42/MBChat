67ce0b971f32c32688df88c175be5a04925a3add
Alan Chandler <alan@chandlerfamily.org.uk>
2008-03-24 14:50:17 +0000

    Change promote to fire any message new moderator may have had queued up

--- promote.php
+++ promote.php
@@ -8,7 +8,7 @@ $puid = $_GET['puid'];
 define ('MBC',1);   //defined so we can control access to some of the files.
 require_once('db.php');
 dbQuery('START TRANSACTION;');
-$result = dbQuery('SELECT uid, name, title, role, rid, moderator FROM users WHERE uid = '.dbMakeSafe($puid).';');
+$result = dbQuery('SELECT uid, name, role, rid, moderator, question  FROM users WHERE uid = '.dbMakeSafe($puid).';');
 if(mysql_num_rows($result) == 0) {
 	dbQuery('ROLLBACK;');
 	die('Release Message - Invalid User id');
@@ -22,10 +22,14 @@ if ($user['role'] == 'M' || $user['role'] == 'S') {
 } else {
 	$mod = $user['role'];
 }
-dbQuery('UPDATE users SET role = "M", moderator = '.dbMakeSafe($mod).', time = NOW() WHERE uid = '.dbMakeSafe($puid).';');
-
+dbQuery('UPDATE users SET role = "M", moderator = '.dbMakeSafe($mod).', time = NOW() , question = NULL WHERE uid = '.dbMakeSafe($puid).';');
 dbQuery('INSERT INTO log (uid, name, role, type, rid) VALUES ('.
 				dbMakeSafe($puid).','.dbMakeSafe($user['name']).', "M" , "RM" ,'.dbMakeSafe($user['rid']).');');
+if ($user['question'] != '' ) {
+	dbQuery('INSERT INTO log (uid name, role, type, rid, message) VALUES ('. 
+				dbMakeSafe($puid).','.dbMakeSafe($user['name']).
+				', "M" , "ME" ,'.dbMakeSafe($user['rid']).','.dbMakeSafe($user['question']).');');
+}
 
 dbQuery('COMMIT ;');
 include('poll.php');  //Get an immediate reply to messages
