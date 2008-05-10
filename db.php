38e52cc187fb69ab46225fa305ddcf68ca9393ed
Alan Chandler <alan@chandlerfamily.org.uk>
2008-05-10 08:00:06 +0100

    Reset password for new database at home

--- db.php
+++ db.php
@@ -8,7 +8,7 @@ header("Expires: Mon, 26 Jul 1997 05:00:00 GMT"); // Date in the past
 	$db_server = 'localhost';
 	$db_name = 'melindas_chat';
 	$db_user = 'melindas_chat';
-	$db_password = 'x5aces42';
+	$db_password = 'xxxxxx';
 	mysql_connect($db_server, $db_user, $db_password) or die('Could not connect to database: ' . mysql_error());
 	mysql_select_db($db_name) or die('Could not select database: '.mysql_error());
 	function dbQuery($sql) {
