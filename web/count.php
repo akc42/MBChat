db3ba120f78b3b23d1c54e178b62a588dbc18c29
Alan Chandler <alan@chandlerfamily.org.uk>
2009-09-07 18:07:03 +0100

    Improve the efficiency of the count mechanism

--- web/count.php
+++ web/count.php
@@ -29,24 +29,12 @@ define('MBCHAT_PATH', dirname($_SERVER['SCRIPT_FILENAME']).'/');
 define ('MBC',1);   //defined so we can control access to some of the files.
 require_once('db.php');
 
-// We want to show colours if Melinda or our Special Guests are in chat
-
-$best = 'R';
-$total = 0;
-$result = dbQuery('SELECT role, count(*) as chatters, users.rid, rooms.type AS type FROM users LEFT JOIN rooms  ON rooms.rid = users.rid WHERE rooms.type IS null OR rooms.type != "C" GROUP BY role;');
-if (mysql_num_rows($result) != 0) {		
-	while ($row = mysql_fetch_assoc($result)) {
-		$total += $row['chatters'];
-		if ($row['role'] == 'H') {
-			$best =  'H';
-		} else if ($row['role'] == 'G' && $best == 'R' ) {
-			$best = 'G' ;
-		}
-	}
+$result = dbQuery('SELECT count(*) as chatters FROM users;');
+if ($row = mysql_fetch_assoc($result)) { 
+    echo $row['chatters'];
+} else {
+    echo '0';
 }
-
 mysql_free_result($result);
-
-echo $total ;
 
 ?>
