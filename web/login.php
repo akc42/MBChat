ca362ee451e80e4062afd5c6f6fd64c428fc7b6f
Alan Chandler <alan@chandlerfamily.org.uk>
2009-09-07 06:57:36 +0100

    Fixup problem whereby when a user is logged out because of a comms timeout his pipe was not deleted.
    When a users comms fail, he does not go through the normal logout
    process which deletes his communications pipe.  So when he connects
    again, he will be immediately logged out in order to delete it.
    
    This commit changes it so that when you detect a timeout, you delete
    the pipe of the user so when he restarts he can restart properly.
    
    In this case you also have to cater for the fact that in the
    Javascript, if you see a LT message (logout with timeout) you try and
    log in again.  In this case you need to recreate the fifo.
    
    There was a 5 second delay in logging out - this has been changed to
    be only 2 seconds.

--- web/login.php
+++ web/login.php
@@ -5,6 +5,8 @@ $uid = $_POST['user'];
 if ($_POST['password'] != sha1("Key".$uid))
 	die('Hacking attempt got: '.$_POST['password'].' expected: '.sha1("Key".$uid));
 
+define('MBCHAT_PATH', dirname($_SERVER['SCRIPT_FILENAME']).'/');
+
 $txt = 'MBchat version - '.$_POST['mbchat'].', Mootools_Version - '.$_POST['version'].' - build - '.$_POST['build'] ;
 $txt .=' Browser - '.$_POST['browser'].' on Platform - '.$_POST['platform'];
 define ('MBC',1);   //defined so we can control access to some of the files.
@@ -17,6 +19,14 @@ if(mysql_num_rows($result) != 0) {
 			dbMakeSafe($uid).','.dbMakeSafe($row['name']).','.dbMakeSafe($row['role']).
 			', "LI" ,'.dbMakeSafe($row['rid']).','.dbMakeSafe($txt).');');
     $lid = mysql_insert_id();
+    
+//If FIFO doesn't exists (when trying to login after timeout for instance) create it
+	if(!file_exists(MBCHAT_PATH."pipes/msg".$uid)) {
+	    $old_umask = umask(0007);
+    	posix_mkfifo(MBCHAT_PATH."pipes/msg".$uid,0660);
+        umask($old_umask);
+    }
+	
 	include_once('send.php');
     send_to_all($lid,$uid, $row['name'],$row['role'],"LI",$row['rid'],'');	
 		
--- web/timeout.php
+++ web/timeout.php
@@ -2,6 +2,7 @@
 if (!defined('MBC'))
 	die('Hacking attempt...');
 
+define('MBCHAT_PATH', dirname($_SERVER['SCRIPT_FILENAME']).'/');
 define('MBCHAT_TIMEOUT_USER',	3); //No of minutes before online user goes offline through lack of activity
 
 $result=dbQuery('SELECT uid, name, role, rid FROM users WHERE NOW() > DATE_ADD(time, INTERVAL '.MBCHAT_TIMEOUT_USER.' MINUTE);');
@@ -11,8 +12,13 @@ if(mysql_num_rows($result) != 0) {
 		dbQuery('INSERT INTO log (uid, name, role, type, rid) VALUES ('.
 			dbMakeSafe($row['uid']).','.dbMakeSafe($row['name']).','.dbMakeSafe($row['role']).
 			', "LT" ,'.dbMakeSafe($row['rid']).');');
+		
 		include_once('send.php');
         send_to_all(mysql_insert_id(),$row['uid'], $row['name'],$row['role'],"LT",$row['rid'],'');	
+
+        unlink(MBCHAT_PATH."pipes/msg".$row['uid']); //Loose FIFO
+
+        
 	}
 };
 mysql_free_result($result);
