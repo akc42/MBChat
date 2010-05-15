4e92c21483cb833f81bc75a7b883757297bb002b
Alan Chandler <alan@chandlerfamily.org.uk>
2010-05-15 22:34:47 +0100

    Remove creation of CHAT URL = not sure we need it

--- supporting/install/writepublickey.php
+++ supporting/install/writepublickey.php
@@ -38,7 +38,6 @@ echo "<?php \n";
 echo "define('RSA_EXPONENT',".hex2dec(bin2hex($key['rsa']['e'])).");\n";
 echo "define('RSA_MODULUS','".hex2dec(bin2hex($key['rsa']['n']))."');\n";
 echo "define('REMOTE_KEY','".$argv[1]."');\n";
-echo "define('CHAT_URL','".$argv[2]."');\n";
 
 
   
