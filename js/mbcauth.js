2fe62a150ee596816d7ab15806c83e74709df9e3
Alan Chandler <alan@chandlerfamily.org.uk>
2010-05-18 06:21:08 +0100

    Update logo and also use uncompressed version of mbcauth
    mbcauth will be compressed automatically in a post commit hook for the
    production branches

--- js/mbcauth.js
+++ js/mbcauth.js
@@ -1,1 +1,121 @@
-function MBCAuth(){var d=new BigInteger(32,new SecureRandom());var e=false;var g=false;if(Browser.Engine.trident&&Browser.Engine.version==5){document.id("rsa_generator").removeClass("loading");document.id("rsa_generator").removeClass("hide");document.id("rsa_generator").set("html",'<span class="error">Internet Explorer V7 is not supported.  Chat will work with Internet Explorer 6 and 8 as well as Firefox, Chrome, Safari and Opera.</span>');return}function b(){if(!e){document.id("rsa_generator").removeClass("loading");document.id("rsa_generator").removeClass("hide");document.id("rsa_generator").set("html",'<span class="error">Security Alert, Server NOT confirmed.  Please notify security</span>')}}var f=function(h){document.id("rsa_generator").addClass("hide");document.id("authblock").removeClass("hide");document.id("login_error").removeClass("hide");if(h){document.id(document.id("login").username).addClass("error");document.id(document.id("login").password).addClass("error")}else{document.id(document.id("login").password).addClass("error")}};var a=new Request.JSON({url:"login/index.php",link:"chain",onComplete:function(h,i){if(h&&h.status){if(h.trial){if(h.trial==d.toString(10)){e=true;coordinator.done("verify",{});a.post.delay(1,this,{user:"$$#",pass:remoteKey})}else{b()}}else{if(h.login&&e){if(h.login.uid==0){g=true;document.id("rsa_generator").addClass("hide");document.id("authblock").removeClass("hide")}else{loginRequestOptions=h.login;coordinator.done("login",{})}}}}else{if(g){f(h.usererror)}}}});var c=d.modPow(new BigInteger(rsaExponent),new BigInteger(rsaModulus));window.addEvent("domready",function(){document.id("login").addEvent("submit",function(j){j.stop();var i={};i.U=document.id("login").username.value;i.P=document.id("login").password.value;if(i.U.contains("$")){f(false);return}if(i.P==""){if(!guestsAllowed){f(false);return}i.P="guest";i.U="$$G"+i.U}var h=(Math.ceil(new Date().getTime()/100000)*100).toString();while(h.length<10){h="0"+h}document.id("rsa_generator").removeClass("hide");document.id("authblock").addClass("hide");document.id("login_error").addClass("hide");document.id(document.id("login").username).removeClass("error");document.id(document.id("login").password).removeClass("error");a.post({user:i.U,pass:hex_md5(i.P+h)})});a.post({user:"$$$",pass:remoteKey,trial:c.toString(10)});b.delay(10000);coordinator.done("dom",{})})};
+/*
+ 	Copyright (c) 2010 Alan Chandler
+    This file is part of MBChat.
+
+    MBChat is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    MBChat is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with MBChat (file COPYING.txt).  If not, see <http://www.gnu.org/licenses/>.
+*/
+function MBCAuth() {
+        var checkNo = new BigInteger(32,new SecureRandom()); 
+        var confirmedServer = false;
+        var internalAuth = false;
+        if(Browser.Engine.trident && Browser.Engine.version == 5) {
+            document.id('rsa_generator').removeClass('loading');
+            document.id('rsa_generator').removeClass('hide');  //just in case
+            document.id('rsa_generator').set('html','<span class="error">Internet Explorer V7 is not supported.  Chat will work with Internet Explorer 6 and 8 as well as Firefox, Chrome, Safari and Opera.</span>');
+            return;
+        }
+             
+        function confirmTimeout() {
+            if(!confirmedServer) {
+                document.id('rsa_generator').removeClass('loading');
+                document.id('rsa_generator').removeClass('hide');  //just in case
+                document.id('rsa_generator').set('html','<span class="error">Security Alert, Server NOT confirmed.  Please notify security</span>');
+            }
+        }
+        var loginError = function(usernameError) { 
+            document.id('rsa_generator').addClass('hide');
+            document.id('authblock').removeClass('hide');
+            document.id('login_error').removeClass('hide');
+            if(usernameError) {
+                document.id(document.id('login').username).addClass('error');
+                document.id(document.id('login').password).addClass('error');
+            } else {
+                document.id(document.id('login').password).addClass('error');
+            }
+        }
+
+        var loginReq = new Request.JSON({
+            url:'login/index.php',
+            link:'chain',
+            onComplete:function(response,t) {
+            if(response && response.status) {
+                if(response.trial) { //responding with the returned security key
+                    if(response.trial == checkNo.toString(10)) {
+                        //matched
+                        confirmedServer = true;
+                        coordinator.done('verify',{});
+                        loginReq.post.delay(1,this,{user:'$$#',pass:remoteKey}); //now find out if I am supposed to prompt
+                    } else {
+                        confirmTimeout();
+                    }
+                } else if (response.login && confirmedServer) {
+                    if(response.login.uid == 0) {//special marker telling me that I must authenticate.
+                        internalAuth = true;  //we are being told to do internal authentication
+                        document.id('rsa_generator').addClass('hide');
+                        document.id('authblock').removeClass('hide');
+                            // and wait for user to respond
+                    } else {
+                        loginRequestOptions = response.login;
+                        coordinator.done('login',{});
+                    }
+                }
+            } else { 
+                if(internalAuth) {
+                    loginError(response.usererror);
+                }
+            }
+            }
+        });
+
+
+
+        var encCheckNo = checkNo.modPow(new BigInteger(rsaExponent),new BigInteger(rsaModulus));
+
+        window.addEvent('domready',function () {
+            document.id('login').addEvent('submit', function(e) {
+                e.stop();
+                var auth = {};
+                auth.U = document.id('login').username.value;
+                auth.P = document.id('login').password.value;
+                if(auth.U.contains('$')) {
+                    loginError(false);
+                    return ;
+                }
+                if(auth.P == '') {
+                    if(!guestsAllowed) {
+                        loginError(false);
+                        return;
+                    }
+                    auth.P = 'guest';
+                    auth.U = '$$G'+auth.U;
+                }
+ 
+                var t1 = (Math.ceil(new Date().getTime()/100000)*100).toString();
+                while(t1.length < 10) {
+                    t1 = '0'+t1;
+                }
+                document.id('rsa_generator').removeClass('hide');
+                document.id('authblock').addClass('hide');
+                document.id('login_error').addClass('hide');
+                document.id(document.id('login').username).removeClass('error');
+                document.id(document.id('login').password).removeClass('error');
+                loginReq.post({user:auth.U,pass:hex_md5(auth.P+t1)});
+            });
+            // This initial request will see if it can authenticate without needing to put up the form - we also want to verify the server
+            loginReq.post({user:'$$$',pass:remoteKey,trial:encCheckNo.toString(10)});
+            confirmTimeout.delay(10000); //give server 10 seconds to come back with correct response.
+            coordinator.done('dom',{});
+        });
+};
+
