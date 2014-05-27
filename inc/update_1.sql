7c4fb5c637edf9fad1b2f44734d42e3eac839011
Alan Chandler <alan@chandlerfamily.org.uk>
2014-05-27 16:37:42 +0100

    Update database setup files for test placement

--- inc/update_1.sql
+++ inc/update_1.sql
@@ -1,3 +1,7 @@
+UPDATE parameters SET value = '/img/emoticons/' WHERE name = 'emoticon_url';
+UPDATE parameters SET value = '/home/alan/dev/mb.com/static/public_html/img/emoticons' WHERE name = 'emoticon_dir';
+
+
 INSERT INTO parameters VALUES 
 ('purge_guest','5',1), --Only used when doing own authentication, days before purging guests.
 ('external','/chat/remote/index.php',1), --url to authenticate if doing external - must be null string to do internal
